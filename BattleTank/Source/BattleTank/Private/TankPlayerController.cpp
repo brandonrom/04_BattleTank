268861bbe231f8f4508ff17d3cfc5d1ce5920d92
brandon roman <personbrandonr@gmail.com>
2017-07-28 22:06:50 -0400

    BT_120 Delegating to Components

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+#include "GameFramework/Pawn.h"
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
