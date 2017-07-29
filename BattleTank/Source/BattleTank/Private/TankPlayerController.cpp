142cd65f0a0ee51537bedc6ce15c2c85991acb68
brandon roman <personbrandonr@gmail.com>
2017-07-28 23:09:01 -0400

    BP_121 Using Virtual and Override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -5,9 +5,27 @@
 #include "GameFramework/Pawn.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+	GetControlledTank();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
-	return Cast<ATank>(GetPawn());
+	ATank* TempTank = nullptr;
+	TempTank = Cast<ATank>(GetPawn());
+	if (TempTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank found %s"), *(TempTank->GetName()));
+		return TempTank;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No tank found"));
+		return TempTank;
+	}
 }
 
 
